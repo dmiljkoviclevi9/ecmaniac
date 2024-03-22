e267254fa2b4826891bd173d017d8bb75b407a42
Damir Miljković <d.miljkovic@levi9.com>
2024-03-22 15:07:33 +0100

    challengeModel.js schema

--- app/api/src/models/challengeModel.js
+++ app/api/src/models/challengeModel.js
@@ -1,37 +1,46 @@
-import mongoose from "mongoose";
-
+import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
+// Test Case Schema
+const testCaseSchema = new Schema({
+  input: {
+    type: [Schema.Types.Mixed], // Array of any type to support multiple parameters of different types
+    required: true
+  },
+  expectedOutput: {
+    type: Schema.Types.Mixed, // Can be any type to support various return values
+    required: true
+  }
+}, { _id: false }); // Disable automatic _id generation for each test
+
+// Challenge Schema
 const challengeSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    description: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    difficulty: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    category: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    creator: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    tests: {
-        type: String,
-        required: true,
-    },
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  difficulty: {
+    type: String,
+    required: true,
+    enum: ['EASY', 'MEDIUM', 'HARD']
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ['FUNDAMENTALS', 'ARRAYS', 'OBJECTS', 'DATES', 'SETS', 'MAPS', 'REGEX', 'RECURSION', 'CLASSES', 'ERRORS']
+  },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  tests: [testCaseSchema] // Incorporating the Test Case Schema
 });
 
 const Challenge = mongoose.model("Challenge", challengeSchema);
